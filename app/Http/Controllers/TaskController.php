068d207774e1bc25fe780944397d8148cce25bd0
heyhuzi <x@heyhuzi.com>
2018-05-09 20:07:12 +0800

    controller

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    public function index()
+    {
+        // code...
+    }
+
+    public function show(Task $tast)
+    {
+        // code...
+    }
+
+    public function store(Request $request)
+    {
+        // code...
+    }
+
+    public function update(Request $request)
+    {
+        // code...
+    }
+
+    public function delete(Request $request)
+    {
+        // code...
+    }
+}
